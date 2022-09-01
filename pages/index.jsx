370e8c64cc9ab98111917791941bd2cce9eee353
OH63U <dmtp8153@gmail.com>
2022-09-01 21:05:15 +0800

    fix

--- pages/index.jsx
+++ pages/index.jsx
@@ -14,7 +14,7 @@ const Home = () => {
     console.log("fetching nfts");
 
     // const api_key = ""
-    const api_key = "gmBrSxN8uBwrorin36qq1-aaJuxfv9h1"
+    const api_key = "vb8E3s_Mr_vHt41LAYAEt6StJezdhq4d"
 
     const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${api_key}/getNFTs/`;
     var requestOptions = {
@@ -43,7 +43,8 @@ const Home = () => {
       var requestOptions = {
         method: 'GET'
       };
-      const api_key = "gmBrSxN8uBwrorin36qq1-aaJuxfv9h1"
+      const api_key = "vb8E3s_Mr_vHt41LAYAEt6StJezdhq4d"
+
 
       const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${api_key}/getNFTsForCollection/`;
       const fetchURL = `${baseURL}?contractAddress=${collection}&withMetadata=${"true"}`;
